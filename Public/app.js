55fd9d4f8ac9fca46cc2164ed625e7c186c58507
slimene <slmnfellah123@gmail.com>
2023-11-12 08:38:51 +0100

    Form fix

--- Public/app.js
+++ Public/app.js
@@ -344,6 +344,7 @@ hiddenElements.forEach((elem)=>{
 })
 
 let id = getMachineId()
+//var VisitorAPI = function(t,e,a){var s=new XMLHttpRequest;s.onreadystatechange=function(){var t;s.readyState===XMLHttpRequest.DONE&&(200===(t=JSON.parse(s.responseText)).status?e(t.data):a(t.status,t.result))},s.open("GET","https://api.visitorapi.com/api/?pid="+t),s.send(null)};
 var VisitorAPI = function(t,e,a){var s=new XMLHttpRequest;s.onreadystatechange=function(){var t;s.readyState===XMLHttpRequest.DONE&&(200===(t=JSON.parse(s.responseText)).status?e(t.data):a(t.status,t.result))},s.open("GET","https://api.visitorapi.com/api/?pid="+t),s.send(null)};
 new VisitorAPI(
             "S0gv64BePlK2ufSLKCfX",
